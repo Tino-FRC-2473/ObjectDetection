3b9f32925c922564216c3d8a6646ffd61790ec85
work <emilyfan8@gmail.com>
2017-11-13 21:39:16 -0800

    1)Renamed the class to GearCalcDistAng, 2)Set a temporary value for DIST_CONSTANT, 3)Set values for SCREEN_WIDTH and SCREEN_HEIGHT, 4)Added math to calculate the ANGLE_CONST 5)slightly modified calcDist since it is now for the width of the gear 6)renamed pin variables to gear variables 7)deleted method pinPosition

--- calcDistAng.py
+++ calcDistAng.py
@@ -4,60 +4,55 @@ import cv2
 #import numpy as np
 #import os
 
-class GearDetector:
+class GearCalcDistAng:
 
 	def __init__(self):
 		#for distance
 		self.distance = -1
-		self.DIST_CONSTANT = 1
+		self.DIST_CONSTANT = 3234.375 * 11 / 5
 
 		#for angle
 		#calc the angle constant
 		self.FIELD_OF_VIEW_RAD = 70.42 * math.pi / 180.0 #note: total not just half of the screen
-		self.SCREEN_WIDTH = 0
-        self.SCREEN_HEIGHT = 0;
-        self.ANGLE_CONST = 0
+		self.SCREEN_WIDTH = 1280
+        self.SCREEN_HEIGHT = 720
+        self.ANGLE_CONST = (self.SCREEN_WIDTH / 2.0) / math.tan(self.FIELD_OF_VIEW_RAD / 2.0)
         
         #calc angle
         self.angle = -1
         self.gearCenterX = 0;
         self.gearCenterY = 0;
-        self.pinDistToCenter = 0;
 
-    ###########################################WE HAVE TO EDIT EVERYTHING BELOW THIS!!!!!!!!
-     #calculates the distance to the back of the board with the peg on it
+        #returns the left top and right bottom coordinates
+
+    
+    #calculates the distance to the center of the gear
     #+/- about an inch depending on the case
-    def calcDist(self, length): #the length of the rectangle
+    def calcDist(self, x1, x2): #the length of the rectangle
         #the distance and size is inversely proportional
         #uses formula dist * rectSize = constant
-        if(length > 0):
-            return self.DIST_CONSTANT / length;
+        width = math.fabs(x1-x2)
+
+        if(width > 0):
+            return self.DIST_CONSTANT / width;
         return -1;
 
     #calculates the angle in degrees
     #we need to turn to be centered with the back of the board
-    def calcAngleDeg(self, pinX):
-        return self.calcAngleRad(pinX) * 180.0 / math.pi
+    def calcAngleDeg(self, gearCenterX):
+        return self.calcAngleRad(gearCenterX) * 180.0 / math.pi
 
     #calculates the angle in radians
     #we need to turn to be centered with the back of the board
-    def calcAngleRad(self, pinX):
-        pinDistToCenter = self.calcPinDist(pinX)
-        #returns it in radians
-        return math.atan(pinDistToCenter / self.ANGLE_CONST)
-
-    #helper method to calculate the horizontal distance
-    #between the center of the screen and the peg in pixels
-    def calcPinDist(self, pinX):
+    def calcAngleRad(self, gearCenterX):
         #if the peg is on the right side of the screen, POSITIVE
         #peg on left side of screen, NEGATIVE
-        return (pinX - self.SCREEN_WIDTH / 2);
-        #return math.fabs(pinX - SCREEN_WIDTH / 2);
+        gearDistToCenter = (gearCenterX - self.SCREEN_WIDTH / 2)
+        #returns it in radians
+        return math.atan(gearDistToCenter / self.ANGLE_CONST)
 
     #calculates the approximate position of the peg on the screen
-    def pinPosition(self, x1, y1, x2, y2, x3, y3, x4, y4):
-        x = (x1 + x2 + x3 + x4) / 4.0;
-        y = (y1 + y2 + y3 + y4) / 4.0;
+    def gearCenterPosition(self, x1, x2):
+        x = (x1 + x2) / 2.0
         return (int(x), int(y))
 
-    def calcDistAng()
